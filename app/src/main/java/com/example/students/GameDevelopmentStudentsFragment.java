3d53c1842d3d43c17428d239ea5c7218d7ba11b5
Simona Nikolova <57250076+simona114@users.noreply.github.com>
2022-12-21 21:10:27 +0200

    Initial commit

---
+++ app/src/main/java/com/example/students/GameDevelopmentStudentsFragment.java
@@ -0,0 +1,29 @@
+package com.example.students;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class GameDevelopmentStudentsFragment extends Fragment {
+
+
+    public GameDevelopmentStudentsFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_game_development_students, container, false);
+    }
+}
