81cf00a1746869e2a8f4349f1ef7a5a6790a67df
Simona Nikolova <57250076+simona114@users.noreply.github.com>
2022-12-21 21:35:35 +0200

    Set up tab layout with viewpager2

--- app/src/main/java/com/example/students/MainActivity.java
+++ app/src/main/java/com/example/students/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.students;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager2 viewPager2 = findViewById(R.id.pager);
+        ScreenSlidePagerAdapter adapter = new ScreenSlidePagerAdapter(this);
+        viewPager2.setAdapter(adapter);
+        TabLayout tabLayout = findViewById(R.id.tab_layout);
+        new TabLayoutMediator(tabLayout, viewPager2,
+                (tab, position) -> {
+                    if (position == 0) {
+                        tab.setText(getString(R.string.specialty_business_informatics));
+                    } else if (position == 1) {
+                        tab.setText(getString(R.string.specialty_computer_programming));
+                    } else {
+                        tab.setText(getString(R.string.specialty_game_development));
+                    }
+                }).attach();
     }
 }
---
+++ app/src/main/java/com/example/students/ScreenSlidePagerAdapter.java
@@ -0,0 +1,34 @@
+package com.example.students;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ScreenSlidePagerAdapter extends FragmentStateAdapter {
+
+    public ScreenSlidePagerAdapter(FragmentActivity fa) {
+        super(fa);
+    }
+
+    private static final int NUM_PAGES = 3;
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        if (position == 0) {
+            return new BusinessInformaticsStudentsFragment();
+        } else if (position == 1) {
+            return new GameDevelopmentStudentsFragment();
+        } else {
+            return new ComputerProgrammingStudentsFragment();
+        }
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return NUM_PAGES;
+    }
+
+}
